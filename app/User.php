35c4eddea3eff693cc0d7f81129b04880ebe7d21
Oladeji Stephen <Oladeji Stephen>
2017-04-25 14:48:48 +0100

    users page working without photos

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','role_id','is_active','photo_id',
     ];
 
     /**
@@ -25,6 +25,6 @@ class User extends Authenticatable
     ];
 
     public function role(){
-        return $this->belongsTo('App|Role');
+        return $this->belongsTo('App\Role');
     }
 }
