0c30d6187a23da29e622ffc705adc4d0f1c7c7f1
Oladeji Stephen <Oladeji Stephen>
2017-05-03 12:04:11 +0100

    able to create posts with dynamic categories

--- app/Post.php
+++ app/Post.php
@@ -21,4 +21,15 @@ class Post extends Model
 
         return $this->belongsTo('App\User');
     }
+
+    public function category(){
+
+        return $this->belongsTo('App\Category');
+
+    }
+
+    public function photo(){
+
+        return $this->belongsTo('App\Photo');
+    }
 }
